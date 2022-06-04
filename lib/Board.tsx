9d3e5847ab573d84d2dc6c7ad00a796cf1c7effd
idbd <idbd.young@gmail.com>
2022-06-04 16:35:50 +0900

    Fix board border width

--- lib/Board.tsx
+++ lib/Board.tsx
@@ -150,7 +150,7 @@ const Board: React.FC<BoardProps> = ({ style, gameBoarderColor }) => {
               (window.innerWidth *
                 (gameOptions.range.end - gameOptions.range.start)) /
               100
-            }`,
+            }%`,
             borderTop: `${gameOptions.difficulty ?? 2}px solid ${
               gameBoarderColor ?? "red"
             }`,
@@ -170,7 +170,7 @@ const Board: React.FC<BoardProps> = ({ style, gameBoarderColor }) => {
               (window.innerWidth *
                 (gameOptions.range.end - gameOptions.range.start)) /
               100
-            }`,
+            }%`,
             borderBottom: `${gameOptions.difficulty ?? 2}px solid ${
               gameBoarderColor ?? "red"
             }`,
